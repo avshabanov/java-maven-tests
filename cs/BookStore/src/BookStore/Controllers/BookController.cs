b8311d09b93c75e32fea3d936ece2d4184f956a4
Alexander Shabanov <avshabanov@gmail.com>
2016-09-30 21:29:43 -0700

    Adding ASP.NET Core 5 sample - BookStore

---
+++ cs/BookStore/src/BookStore/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [Route("api/book")]
+    public class BookController : Controller
+    {
+        readonly IEnumerable<Book> books = new Book[]
+        {
+            new Book() { Id = "1", Name = "Far Rainbow", Created = DateTime.Today, ReferralCount = 347 },
+            new Book() { Id = "2", Name = "Hard to be a God", Created = DateTime.Today, ReferralCount = 905 },
+            new Book() { Id = "3", Name = "Ugly Swans", Created = DateTime.Today, ReferralCount = 561 }
+        };
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return books;
+        }
+
+        [HttpGet("{id}", Name = "GetBook")]
+        public IActionResult GetById(string id)
+        {
+            var book = books.FirstOrDefault(b =>
+            {
+                return b.Id.Equals(id);
+            });
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(book);
+        }
+    }
+}
