3aae35981db3f81f41132847a7298379eb0ae1fc
Alexander Shabanov <avshabanov@gmail.com>
2021-02-10 23:48:03 -0800

    Add dummy log generator for splunk lesson

---
+++ apps/loggen/dummy_json_log_gen.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+
+# Dummy structured log generator
+# Sample use:
+#   bash dummy_json_log_gen.sh | jq
+
+while true
+do
+  DATE=$(echo `date +%Y-%m-%d\ %T.%Z`)
+  # GUID:
+  #ID=$(od -x /dev/urandom | head -1 | awk '{OFS="-"; print $2$3,$4,$5,$6,$7$8$9}')
+  ID=$(od -x /dev/urandom | head -1 | awk '{OFS=""; print $2$3}')
+  SYSTEM_DATA=$(top -bn1 | head -8 | tail -1)
+  SYSTEM_INFO=$(echo $SYSTEM_DATA | awk '{OFS=" "; print "\"pid\": "$1", \"user\": \""$2"\", \"cpu\": "$9", \"virt\": "$5", \"mem\": "$10", \"time-delta\": \""$11"\""}')
+
+  echo "{\"date\": \"$DATE\", \"request\": \"$ID\", $SYSTEM_INFO}"
+
+  sleep $(echo $(($(shuf -i 1-200 -n 1)/100)).$(($(shuf -i 1-100 -n 1))))
+done
+
+exit 0
