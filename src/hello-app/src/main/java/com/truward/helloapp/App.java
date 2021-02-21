874bb65f60a029e04d43658b075fa8e4cc3722f6
Alexander Shabanov <avshabanov@gmail.com>
2021-02-20 18:45:18 -0800

    Update some dependencies

--- src/hello-app/src/main/java/com/truward/helloapp/App.java
+++ src/hello-app/src/main/java/com/truward/helloapp/App.java
@@ -1,11 +1,8 @@
 package com.truward.helloapp;
 
-/**
- * Hello world!
- *
- */
 public class App {
-    public static void main(String[] args) {
+
+  public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
